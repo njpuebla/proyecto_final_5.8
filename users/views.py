954a617c931de02898d5580c78724811c8e7eb9f
Alejandro <93834064+aleramirez1980@users.noreply.github.com>
2024-08-09 22:18:54 -0300

    Login y logout

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login,authenticate
+from django.contrib.auth.views import LogoutView
 
 def login_request(request):
 
